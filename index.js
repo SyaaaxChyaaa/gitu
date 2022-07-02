434c33a55890c53d7162cd6d1f9957c6faaa6e5d
SyaaaxChyaaa <107969718+SyaaaxChyaaa@users.noreply.github.com>
2022-07-02 14:13:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const fetch = require('node-fetch');
 const moment = require('moment');
 const chalk = require('chalk');
 const rs = require('readline-sync');
-const delay = require('delay');
+const delay = require('delay')
 
-const GoStumble = (auth) => new Promise((resolve, reject) => {
+console.log(`${chalk.white(`
+1 Push Starpass
+2 Push Tropi
+3 Push Mahkota
+Choose to use the available number`)} 
+`);
+const round = rs.question(`[+] Pilih nomor dari 1 sampai 3 yang ingin dipakai  : `);
+    console.log('');
+    
+    const GoStumble = (auth) => new Promise((resolve, reject) => {
 
-  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/'+round, {
     method: 'GET',
     headers: {
       'authorization': auth
@@ -24,14 +33,14 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
 
 (async () => {
 
-  console.log(`Stumble Script Hack Crown Ril 100% 😱😱😅😅 
-Script Made By : ${chalk.bold('Rexxy')} - Helper : No Helper☝️😅
+  console.log(`Script Stumble Supaya Langsung Jadi Top 1 Satir Di Stumble Guys Anjvmer
+Script Made By : ${chalk.bold('Babang Tamvan Rexxy')} - Helper : @FiqzGantengBanget & @AlvinMandulAnjme☝️😅
 `);
 
-const auth = rs.question('Tulis Auth Kode Lu Disini Anjimer! : ');
+  const auth = rs.question('Tulis Auth Kode Lu Disini Anjimer! : ');
   console.log('');
 
-while (true) {
+  while (true) {
 
     const result = await GoStumble(auth);
     if (!result) {
@@ -44,15 +53,17 @@ while (true) {
       const data = JSON.parse(result);
       const username = data.User.Username;
       const country = data.User.Country;
-      const exp = data.User.Experience;
-      const tokenPass = data.User.BattlePass.PassTokens;
       const trophy = data.User.SkillRating;
       const crown = data.User.Crowns;
+      const exp = data.User.Experience;
+      const tokenPass = data.User.BattlePass.PassTokens
 
-console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.green(`User : ${username}`)} | ${chalk.blue(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)} | ${chalk.green(`Exp : ${exp}`)} | ${chalk.blue(`Country : ${country}`)} | ${chalk.bgGreen(`StarPass : ${tokenPass}`)}`))
-      await delay(3000);
+console.log(chalk.bgBlack(`\r[ ${moment().format('hh:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)} | ${chalk.blue(`Exp : ${exp}`)} | 
+${chalk.redBright(`tokenPass : ${tokenPass}`)} |
+${chalk.greenBright(`Country : ${country}`)}`))
+      await delay(4000);
 
-       } else if (result == 'BANNED') {
+    } else if (result == 'BANNED') {
         console.log(chalk.bgRed(`Akunlu Keban Titit Makanya Ga Ush Ngescript 🤬🤬🤬🤬`));
      break;
     }
